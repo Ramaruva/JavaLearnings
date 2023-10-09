f0087daf70fed93a5c22c5056fbb06117ab8033c
Ramaruva <ramaruva54@gmail.com>
2023-10-09 10:51:22 -0500

    new branch

--- firstProject/src/firstProject/Main.java
+++ firstProject/src/firstProject/Main.java
@@ -3,5 +3,18 @@ package firstProject;
 public class Main {
    public static void main(String [] args) {
 	   System.out.println("Hello world");
+	   int a = 0;
+	   System.out.println(a);
+	   String name = new String("example");
+	   System.out.println(name);
+	   StringBuffer demostring = new StringBuffer("Hello buffer");
+	   System.out.println(demostring);
+	   String nam= "Pranita";
+	  nam= nam.concat("good");
+	   System.out.println(nam);
    }
 }
+
+//String are immutable  ie it can't be changed
+// String is a peer class of string that provides much of functionality of strings
+//
