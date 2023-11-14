0363ac8aff397c2ff6973ff258448cf9f19af271
Ramaruva <ramaruva54@gmail.com>
2023-11-14 15:53:40 -0600

    map intro

--- firstProject/src/firstProject/Mappract.java
+++ firstProject/src/firstProject/Mappract.java
@@ -1,7 +1,14 @@
 package firstProject;
 /*
  * map practice 
- * small change
+ * A map defines mappings  from key to values
+ * The <key, value> pair is called 	an enty
+ * a map  does not allow duplicate keys, in other words, the keys are unique
+ * BOth the keys and the values must be objects, with primitive values being
+ * wrapped in their respective primitve wrapper objects when they are put in a map
+ * methods 
+ * 
+ * 
  */
 public class Mappract {
 
